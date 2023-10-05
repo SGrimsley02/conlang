4bcb39b6940a50fe53984a62943c1ce52ac615d9
SGrimsley02 <sam.grimsley@gmail.com>
2023-10-05 12:56:10 -0500

    Started work on HTML implementation

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('InputsPage.html')
+
+@app.route('/process_input', methods=['POST'])
+def process_input():
+    Word = request.form.get('Word')
+    Ipa = request.form.get('IPA')
+    PoS = request.form.get('PoS')
+    Definition = request.form.get('Definition')
+    Translation = request.form.get('Translation')
+    Example = request.form.get('Example')
+    Synonym = request.form.get('Synonym')
+    Antonym = request.form.get('Antonym')
+
+    # Now you can use these variables in your Python code
+    print(f"Input 1: {Word}")
+    print(f"Input 2: {Ipa}")
+    print(f"Input 3: {PoS}")
+    print(f"Input 4: {Definition}")
+    print(f"Input 5: {Translation}")
+    print(f"Input 6: {Example}")
+    print(f"Input 7: {Synonym}")
+    print(f"Input 8: {Antonym}")
+
+    return "Inputs processed successfully"
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
